46795e7cc61c2bbae7b3e1e50468d61354d56141
francescorossi1 <rossi.francesco.dev@gmail.com>
2022-07-04 18:09:32 +0200

    completed

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@
 // Hardcodiamo tutto in pagina e poi facciamo scrivere a js
 
 const display = document.getElementById('display');
-const countdown = document.getElementById('countdown');
+const countdownDisplay = document.getElementById('countdown');
+let messageDisplay = document.getElementById('message');
 const numbers = [];
-let count = 30;
+const userNumbers = [];
+let count = 5;
 
 // getRandomNumber
 
@@ -13,11 +15,47 @@ const getRandomNumber = (minNum, maxNum) => {
     return Math.floor(Math.random() * (maxNum + 1 - minNum)) + minNum;
 }
 
+// countDown
+
+const countDown = setInterval(() => {
+    countdownDisplay.innerText = count;
+    if(count === 0){
+        display.style.display = "none";
+        countdownDisplay.style.display = "none";        
+    }else if(count < 0){
+        clearInterval(countDown);
+        askUserNumbers();
+    }
+    count--;
+},1000)
+
+// askUserNumbers
+
+const askUserNumbers = () => {
+    let score = 0;
+    let message = 'Numeri corretti: ';
+    for(let i = 1; i <= numbers.length; i++){
+        let userNumber = parseInt(prompt(`Scrivi il ${i} numero!`));
+        while((isNaN(userNumber) || userNumber < 1 || userNumber > 100)){
+            userNumber = parseInt(prompt(`Usa un numero corretto. Scrivi il ${i} numero!`));
+        }
+        userNumbers.push(userNumber);
+        console.log(userNumber)
+        console.log(numbers)
+        if(numbers.includes(userNumber)){
+            score = score + 1;
+            message += `${userNumber} `
+        }
+    }
+    message += `Punteggio finale: ${score}`
+    messageDisplay.innerHTML = message;
+}
+
 // Montiamo il contenuto da visualizzare
 
 display.innerHTML += `<div class="my-3">Memorizza i seguenti numeri:</div><div>`
 
-for(let i = 0; i < 5; i++){
+for(let i = 1; i <= 5; i++){
     let randomNum = getRandomNumber(1,100);
     while(numbers.includes(randomNum)){
         randomNum = getRandomNumber(1,100);
@@ -27,4 +65,3 @@ for(let i = 0; i < 5; i++){
 }
 
 display.innerHTML += `</div><div class="my-3">I numeri scompariranno fra:</div>`
-countdown.innerText = count;
